8e33efe6f5433ca54def98994dee7c438d47a309
Letilu <letyfunes@gmail.com>
2020-12-29 21:06:34 -0300

    borrar líneas de comparación con solución

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -62,20 +62,7 @@ function agregarStringInvertida() {
 
 }
 
-function agregarStringInvertida() {
-  // Agrega un método al prototype de String que devuelva la misma cadena de caracteres, pero invertida.
-  // El método debe llamarse "reverse"
-  // Ej: 'menem'.reverse() => menem
-  // 'toni'.reverse() => 'inot'
-  // Pista: Necesitarás usar "this" dentro de "reverse"
-  String.prototype.reverse = function() {
-    var stringInvertida = '';
-    for(var i = this.length - 1; i>=0; i--) {
-      stringInvertida = stringInvertida + this.charAt(i);
-    }
-    return stringInvertida;
-  };
-}
+
 
 
 
